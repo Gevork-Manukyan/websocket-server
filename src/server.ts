ecabe145e010d370fb51b757517673ba1b2d3acf
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-01-09 22:46:00 -0800

    creates leave-room and disconnect enpoints

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express'; // Import Express
 import http from 'http'; // Import Node.js HTTP module
-import { Server, Socket } from 'socket.io'; // Import types from Socket.IO
+import { Server } from 'socket.io'; // Import types from Socket.IO
 import { PORT } from './utils/constants';
+import { CurrentGames, gameId, Player } from './utils/types';
 
 const app = express()
 const server = http.createServer(); // Create an HTTP server
@@ -20,16 +21,28 @@ const io = new Server(server, {
     },
 });
 
+
+
+const currentGames: CurrentGames = {}
+
 // Creates the gameplay namespace that will handle all gameplay connections
 const gameNamespace = io.of("/gameplay")
-
 gameNamespace.on('connection', (socket) => {
     console.log('A player connected to the gameplay namespace');
 
-    // Join Game Room
-    socket.on('join-game', (gameId) => {
+    socket.on('join-game', (gameId: gameId) => {
         socket.join(gameId); // Creates room if doesn't exist
         console.log(`Player joined game room: ${gameId}`);
+    })
+
+    socket.on("leave-room", (gameId: gameId, playerId: Player["id"]) => {
+        const newPlayerList = currentGames[gameId].players.filter(player => player.id !== playerId);
+        currentGames[gameId] = { ...currentGames[gameId], players: newPlayerList }
+        socket.leave(gameId);
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`Player disconnected from gameplay namespace`);
     })
 })
 
