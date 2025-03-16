f27ff690f2ea4c62a363d4b6f6d435e553b6bd7b
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-03-15 21:28:14 -0700

    refactor: add MongoDBService and gameStateManager imports to index for improved module accessibility

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,9 @@
+import { gameStateManager } from './GameStateManager';
+import { MongoDBService } from './MongoDBService';
+
+export const mongoDBService = MongoDBService.getInstance();
+export { gameStateManager };
+
 export * from './GameStateManager';
 export * from './GameEventEmitter';
 export * from './CustomError/BaseError';
