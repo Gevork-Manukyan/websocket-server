4bb3582532d1d3d95442ee01f9905f365cb5e062
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-03-22 17:32:28 -0700

    refactor db-model for battlefield

--- src/models/Battlefield/db-model.ts
+++ src/models/Battlefield/db-model.ts
@@ -3,48 +3,39 @@ import { ElementalCard } from '../../types';
 import { SpaceOption } from '../../types/types';
 
 // Define the base types
+type BattlefieldSpaceBase = {
+    spaceNumber: SpaceOption;
+    value: ElementalCard | null;
+    connections: {
+        TL?: number | null;  // Index in the fieldArray
+        T?: number | null;
+        TR?: number | null;
+        L?: number | null;
+        R?: number | null;
+        BL?: number | null;
+        B?: number | null;
+        BR?: number | null;
+    };
+};
+
 type BattlefieldBase = {
     fieldArray: BattlefieldSpaceBase[];
     numPlayersOnTeam: 1 | 2;
 };
 
-type BattlefieldSpaceBase = {
-    spaceNumber: SpaceOption;
-    value: ElementalCard | null;
-    connections: {
-        TL?: string | null;
-        T?: string | null;
-        TR?: string | null;
-        L?: string | null;
-        R?: string | null;
-        BL?: string | null;
-        B?: string | null;
-        BR?: string | null;
-    };
-};
-
 // Extend Document for MongoDB
-export interface IBattlefield extends Document, BattlefieldBase {}
-
 export interface IBattlefieldSpace extends Document, BattlefieldSpaceBase {}
 
-// Define the schemas
-export const BattlefieldSchema = new Schema<IBattlefield>({
-    fieldArray: [{ type: Schema.Types.Mixed, required: true }],
-    numPlayersOnTeam: { type: Number, required: true, enum: [1, 2] }
-});
+export interface IBattlefield extends Document, BattlefieldBase {}
 
+// Define the schemas
 export const BattlefieldSpaceSchema = new Schema<IBattlefieldSpace>({
     spaceNumber: { type: Number, required: true },
     value: { type: Schema.Types.Mixed, required: true, default: null },
-    connections: {
-        TL: { type: Schema.Types.Mixed, default: null },
-        T: { type: Schema.Types.Mixed, default: null },
-        TR: { type: Schema.Types.Mixed, default: null },
-        L: { type: Schema.Types.Mixed, default: null },
-        R: { type: Schema.Types.Mixed, default: null },
-        BL: { type: Schema.Types.Mixed, default: null },
-        B: { type: Schema.Types.Mixed, default: null },
-        BR: { type: Schema.Types.Mixed, default: null }
-    }
+    connections: { type: Schema.Types.Mixed, required: true, default: null }
+});
+
+export const BattlefieldSchema = new Schema<IBattlefield>({
+    fieldArray: { type: [BattlefieldSpaceSchema], required: true },
+    numPlayersOnTeam: { type: Number, required: true, enum: [1, 2] }
 });
