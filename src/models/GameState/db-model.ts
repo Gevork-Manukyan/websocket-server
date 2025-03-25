7cc27b6c9deb9e5300cfdb0cf8da72066bc43b6f
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-03-25 11:07:48 -0700

    exports GameState model

--- src/models/GameState/db-model.ts
+++ src/models/GameState/db-model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 import { gameId, Transition } from '../../types';
 
 type GameStateBase = {
@@ -14,3 +14,5 @@ export const GameStateSchema = new Schema({
     stateTransitionTable: [{ type: Schema.Types.Mixed, required: true }],
     currentTransition: { type: Schema.Types.Mixed, required: true },
 }); 
+
+export const GameState = model<IGameState>('GameState', GameStateSchema);
